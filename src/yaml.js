9278828cfd2409448444c62dabf13451aa35a743
costa.h4evr <costa.h4evr@gmail.com>
2011-04-03 11:08:18 +0000

    Changed the creation of the XMLHTTPRequest so that it works in IE too!

--- src/yaml.js
+++ src/yaml.js
@@ -66,15 +66,44 @@ var YAML =
         };
     }
 
+    // function to create an XMLHttpClient in a cross-browser manner
+    function createXMLHTTPRequest() {
+        var xmlhttp;
+        
+        try {
+            // Mozilla / Safari / IE7
+            xmlhttp = new XMLHttpRequest();
+        } catch (e) {
+            // IE
+            var XMLHTTP_IDS = new Array('MSXML2.XMLHTTP.5.0',
+            'MSXML2.XMLHTTP.4.0',
+            'MSXML2.XMLHTTP.3.0',
+            'MSXML2.XMLHTTP',
+            'Microsoft.XMLHTTP' );
+            var success = false;
+            for (var i=0;i < XMLHTTP_IDS.length && !success; i++) {
+                try {
+                xmlhttp = new ActiveXObject(XMLHTTP_IDS[i]);
+                success = true;
+                } catch (e) {}
+            }
+            if (!success) {
+                throw new Error('Unable to create XMLHttpRequest.');
+            }
+        }
+        
+        return xmlhttp;
+    }
+
     function fromURL(src, ondone) {
-        var client = new XMLHttpRequest();
-        client.open('GET', src);
+        var client = createXMLHTTPRequest();
         client.onreadystatechange = function() {
-            if (client.readyState == 4 || client.status == 200) {
-                var txt = client.responseText;
+            if (this.readyState == 4 || this.status == 200) {
+                var txt = this.responseText;
                 ondone(YAML.eval(txt));
             }
         };
+        client.open('GET', src);
         client.send();
     }
 
